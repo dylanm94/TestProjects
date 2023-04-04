ff95be73a6502fa5b001b382d2e7e3f9cadd7b52
dylanm94 <71091181+dylanm94@users.noreply.github.com>
2023-04-04 13:00:18 +0200

    commit

---
+++ TestProject01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TestProject01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Balgehakt";
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WindowHeight = 50;
+
+
+            Console.WriteLine("Hello, What is your name?");
+            string input = Console.ReadLine();
+            Console.WriteLine("Hello " + input + "!");
+            Console.WriteLine("Is you name correct?");
+            string answer = Console.ReadLine();
+            bool namecheck;
+            if (answer == "yes" || answer == "Yes")
+            {
+                namecheck = true;
+            }
+            else if (answer == "no" || answer == "No")
+            {
+                namecheck = true;
+            }
+            else
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
